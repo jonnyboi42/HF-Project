ba2c8a8348a3180a052df19ba4bbdbee1cfee39f
jonnyboi42 <connelljon9@gmail.com>
2024-03-22 21:58:55 -0500

    Fixed Y-axis values

--- mainContentScript.js
+++ mainContentScript.js
@@ -1,10 +1,10 @@
-
+  
 // Generate random spending data for each month
 function generateRandomSpending() {
     const spending = [];
     for (let i = 0; i < 12; i++) {
-        // Generate a random spending amount between 1500 and 2500
-        const amount = Math.floor(Math.random() * (2500 - 1500 + 1)) + 1500;
+        // Generate a random spending amount between 1500 and 3000
+        const amount = Math.floor(Math.random() * (3000 - 1500 + 1)) + 1500;
         spending.push(amount);
     }
     return spending;
@@ -13,24 +13,32 @@ const ctx = document.getElementById('myChart');
 
 
 new Chart(ctx, {
-type: 'line',
-
-data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Novermber', 'December'],
-    datasets: [{
-    label: 'Monthly Spending',
-    data: generateRandomSpending(),
-    borderWidth: 1
-    }]
-},
-options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-    y: {
-        beginAtZero: true
+    type: 'line',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Novermber', 'December'],
+        datasets: [{
+            label: 'Monthly Spending',
+            data: generateRandomSpending(),
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+                beginAtZero: false,
+                ticks: {
+                    callback: function(value) {
+                        return '$' + value;
+                    }
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            },
+        }
     }
-    }
-}
 });
-  
